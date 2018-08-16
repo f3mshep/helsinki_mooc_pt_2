7d5aecc4608fe432b58a73dbcae6a615814f4065
f3mshep <superbiscuit@gmail.com>
2018-08-16 16:55:17 -0700

    working on FarmSimulator

---
+++ week10-week10_31.FarmSimulator/src/farmsimulator/BulkTank.java
@@ -0,0 +1,42 @@
+package farmsimulator;
+
+public class BulkTank {
+
+    private double capacity, volume;
+
+    public BulkTank(){
+        this.capacity = 2000.0;
+    }
+    public BulkTank(double capacity){
+        this.capacity = capacity;
+    }
+    public double getCapacity(){
+        return this.capacity;
+    }
+
+    public double getVolume(){
+        return this.volume;
+    }
+
+    public double howMuchFreeSpace(){
+        return this.capacity - this.volume;
+    }
+
+    public void addToTank(double amount){
+        if (amount + volume > capacity) volume = capacity;
+        else volume += amount;
+    }
+
+    public double getFromTank(double amount){
+        double amountTaken;
+        if (amount > volume) amountTaken = volume;
+        else amountTaken = amount;
+        this.volume -= amountTaken;
+        return amountTaken;
+    }
+
+    @Override
+    public String toString() {
+        return  Math.ceil(this.volume) + "/" + this.capacity;
+    }
+}
