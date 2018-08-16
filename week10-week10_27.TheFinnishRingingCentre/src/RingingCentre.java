0d4a0b3e8e812515b715aae43188a157961bdfe2
f3mshep <superbiscuit@gmail.com>
2018-08-16 11:49:36 -0700

    solution for RingingCentre

--- week10-week10_27.TheFinnishRingingCentre/src/RingingCentre.java
+++ week10-week10_27.TheFinnishRingingCentre/src/RingingCentre.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RingingCentre {
+
+    private Map<Bird, List<String>> sightings = new HashMap<Bird, List<String>>();
+
+    public void observe(Bird bird, String location){
+        List<String> locations = sightings.get(bird);
+        if (locations == null){
+            locations = new ArrayList<String>();
+            sightings.put(bird, locations);
+        }
+        locations.add(location);
+    }
+
+    public void observations(Bird bird){
+        List<String> locations = sightings.get(bird);
+        if (locations == null) System.out.println(bird + " observations: 0");
+        else {
+            System.out.println(bird + " observations: " + locations.size());
+            for (String location : locations){
+                System.out.println(location);
+            }
+        }
+    }
+
 }
